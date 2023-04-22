ac125e6421fe28f3a1fa68de9bf14c11b25e278c
Brian Obot <brianobot9@gmail.com>
2023-04-22 16:50:40 +0100

    Update codebase

--- acid_viscosity.py
+++ acid_viscosity.py
@@ -11,19 +11,19 @@ import numpy as np
 import pandas as pd
 from scipy.interpolate import interp1d
 from matplotlib import pyplot as plt
+from datetime import datetime
 
-
+# Program Flow 
 # plot original data on graph
-# - figure title
-# - label x coordinate
-# - label y coordinate
-# - provide legend for the different conc chart lines
+# - figure title ✅
+# - label x coordinate ✅
+# - label y coordinate ✅
+# - provide legend for the different conc chart lines ✅
 
 # obtain regression equation for the x,y set
 #  - produce regression for each given conc provided
 
 # use the regression equation to produce a updated graph
-
 # receive temp and produce corresponding acid viscosity for the stated conc
 
 
@@ -31,35 +31,51 @@ filename = "data/acid_viscosity.csv"
 
 data = pd.read_csv(filename, delimiter=",")
 
+# obtain values for the named columns
+temp_range = data['Temp (oF)']
+water_visc = data['Water']
+acid_at_5p = data['5% HCL']
+acid_at_10 = data['10% HCL']
+acid_at_15 = data['15% HCL']
+acid_at_31 = data['31.45% HCL']
 
-def main():
-    """Main entry point to module execution"""
-    print("Data = ", data)
 
-    # obtain values for the named columns
-    temp_range = data['Temp (oF)']
-    water_visc = data['Water']
-    acid_at_5p = data['5% HCL']
-    acid_at_10 = data['10% HCL']
-    acid_at_15 = data['15% HCL']
-    acid_at_31 = data['31.45% HCL']
-
+def plot_original_data():
     # prepare plot data for original data
     plt.plot(temp_range, water_visc, label='water', marker='o')
     plt.plot(temp_range, acid_at_5p, label='5% Hcl', marker='o')
     plt.plot(temp_range, acid_at_10, label='10% Hcl', marker='o')
     plt.plot(temp_range, acid_at_15, label='15% Hcl', marker='o')
     plt.plot(temp_range, acid_at_31, label='31.45% Hcl', marker='o')
-
     # prepare graph metadata for original data chart
     plt.title("Acid Viscosity with Temperature Chart")
     plt.xlabel("Temperature (oF)")
     plt.ylabel("Acid Viscosity (cp)")
     plt.grid(which='both')
     plt.legend()
-
     # start the plot here
     plt.show()
+
+
+def generate_regression_values_for_conc(all=False):
+    trial = 0
+    index = None
+    if all:
+        pass
+    else:
+        options = [5, 10, 15, 31.5]
+        while trial < 3:
+            conc = float(input(f"Please specify a value for the acid concentration: {options=}: "))
+            if conc not in options:
+                trial += 1
+                print("Invalid Value for Concentration! Must be between 0 and 35.\n")
+            else:
+                break
+        else:
+            raise Exception("Too Many Failed Inputs for Conc! Restart Program")
+        
+        print(f"Acid Concentration to be used = {conc}")
+        index = options.index(conc)
 
     # interpolate for the different equations
     interp_water = interp1d(temp_range, water_visc, kind='quadratic', fill_value='extrapolate')
@@ -77,46 +93,71 @@ def main():
     new_acid_15 = interp_acid15(new_x)
     new_acid_31 = interp_acid31(new_x)
 
-    temp_range = new_x
+    return [new_water, new_acid_5, new_acid_10, new_acid_15, new_acid_31], index
+
+
+def plot_new_regression_graph(all=True):
+    if all:
+        data, _ = generate_regression_values_for_conc(all=True)
+    else:
+        data, data_index = generate_regression_values_for_conc()
+        data = [data[data_index], ]
 
     # plot new values
-    plt.plot(temp_range, new_water, marker=",")
-    plt.plot(temp_range, new_acid_5, marker=",")
-    plt.plot(temp_range, new_acid_10, marker=",")
-    plt.plot(temp_range, new_acid_15, marker=",")
-    plt.plot(temp_range, new_acid_31, marker=",")
-    
+    for index, datum in enumerate(data):
+        plt.plot(temp_range, data[index], marker=",")
+    # prepare graph metadata for original data chart
+    plt.title("New Acid Viscosity with Temperature Chart")
+    plt.xlabel("Temperature (oF)")
+    plt.ylabel("New Acid Viscosity (cp)")
+    plt.grid(which='both')
+    plt.legend()
+    # plot the graph here
     plt.show()
 
-    # get regression from interpolated variables
-    degree = 2 # set the degree to be used for the regression equation
-    y_estimates = {
-        "Water": np.polyfit(temp_range, new_water, degree),
-        "Acid 5%": np.polyfit(temp_range, new_acid_5, degree),
-        "Acid 10%": np.polyfit(temp_range, new_acid_10, degree),
-        "Acid 15%": np.polyfit(temp_range, new_acid_15, degree),
-        "Acid 31%": np.polyfit(temp_range, new_acid_31, degree),
-    }
-     
-    f_regression_equations = {}
-    
-    #get formatted regression equation
-    for variable, y_estimate in y_estimates.items():
-        regression_eqn_str = ""
-        index = len(y_estimate)
-        for value in y_estimate:
-            index -= 1
-            if index == 0:
-                regression_eqn_str += f"({value:.2f})X^{index}"
-                continue
-            regression_eqn_str += f" + ({value:.2f})X^{index}"
-    
-            print(f"Regression Equation = {regression_eqn_str}")
-            f_regression_equations[variable] = regression_eqn_str
 
-    print("##########################################################################################")
-    for element, eqn in f_regression_equations.items():
-        print(f"{element:<10} : {eqn}")
+def main():
+    """Main entry point to module execution"""
+    intro = f"""
+    ###################################################################
+    Program Started Successfully!.              Timestamp:{datetime.now().date()}
+    ###################################################################
+    Program to analyze Acid Viscosity for different concentration of 
+    Acid over a temperature temperture Range. Follow the prompt to use
+    the program correctly, 
+    
+    # _____________________________________________
+    NOTE: confirm your inputs before pressing Enter.
+
+    Options (Commands):
+        Type 1: To Plot Original Viscosity-Temperature data for All specified Concentration.
+        Type 2: To Plot the Regression Value for the Viscosity-Temp Data for all the Specified Concentration
+        Type 3: To Plot the Regression Value for the Viscosity-Temp Data for a Specific Concentration
+        Type 4: To Generate a regression equation for a specific concenration.
+        Type 5: To Interatively Obtain a Viscosity Value for a Given Temperature Value
+    
+    _____________________________________________
+    """
+    print(intro)
+    command = None
+    while command != "q":
+        command = input("Enter Command >>>")
+        match command:
+            case "1":
+                plot_original_data()
+            case "2":
+                plot_new_regression_graph()
+            case "3":
+                plot_new_regression_graph(all=False)
+            case "4":
+                data, index = generate_regression_values_for_conc()
+                print(f"Values = {data[index]}")
+            case "5":
+                pass
+            case _:
+                print("Invalid Option! Try Again")
+    
+    print("\nProgram CLOSED Successfully!")
 
 
 if __name__ == "__main__":
